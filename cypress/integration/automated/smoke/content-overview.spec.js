2ebaaf29d93f8cd9f2b185daffe79fa37197d250
Alex Finnarn <alex.finnarn@gmail.com>
2021-01-13 12:37:17 -0600

    try CI now with some test refactoring

--- cypress/integration/automated/smoke/content-overview.spec.js
+++ cypress/integration/automated/smoke/content-overview.spec.js
@@ -1,8 +1,14 @@
 const { idText, createYield } = require('typescript');
 
 describe('Content Overview Tests', function () {
-  it('Can access IR20 content overview', () => {
+  beforeEach(() => {
+    // Test on the IR20 space.
     cy.visit('/admin/ir20/content');
-    cy.get('h1').contains('Content Overview for the ir20 space');
+  });
+
+  context('Can access IR20 content overview', () => {
+    it('should include the space in the page title', () => {
+      cy.get('h1').contains('Content Overview for the ir20 space');
+    });
   });
 });
