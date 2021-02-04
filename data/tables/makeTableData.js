f1173530196d34f763d15fd721208256480e4661
Alex Finnarn <alex.finnarn@gmail.com>
2021-02-04 14:45:06 -0600

    cleaned up some eslint complaints...but not all

--- data/tables/makeTableData.js
+++ data/tables/makeTableData.js
@@ -23,7 +23,7 @@ const newPost = () => {
 export default function makeData(...lens) {
   const makeDataLevel = (depth = 0) => {
     const len = lens[depth];
-    return range(len).map((d) => {
+    return range(len).map(() => {
       return {
         ...newPost(),
         subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
